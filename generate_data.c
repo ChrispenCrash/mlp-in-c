556a1e65b80cc1a6b0b3fe5444176a5aabcd811d
Chris Cashman <chris.cashman91@gmail.com>
2024-06-08 18:34:45 +0100

    Added jitter function to add random noise to sin data

--- generate_data.c
+++ generate_data.c
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double generateGaussianNoise(double mean, double stdDev) {
+    static int hasSpare = 0;
+    static double spare;
+
+    if (hasSpare) {
+        hasSpare = 0;
+        return mean + stdDev * spare;
+    }
+
+    hasSpare = 1;
+    double u, v, s;
+    do {
+        u = (rand() / ((double)RAND_MAX)) * 2.0 - 1.0;
+        v = (rand() / ((double)RAND_MAX)) * 2.0 - 1.0;
+        s = u * u + v * v;
+    } while (s >= 1.0 || s == 0.0);
+
+    s = sqrt(-2.0 * log(s) / s);
+    spare = v * s;
+    return mean + stdDev * u * s;
+}
+
 void generate_sin_data(char *filename) {
     FILE *fptr;
     fptr = fopen(filename, "w");
@@ -11,9 +33,13 @@ void generate_sin_data(char *filename) {
     }
 
     fprintf(fptr, "x,y\n");
+    double mean = 0.0;
+    double stdDev = 0.1;
 
     for (double i = 0.0; i <= 10.0; i += 0.05) {
-        fprintf(fptr, "%.1f,%.8f\n", i, sin(i));
+        double sineValue = sin(i);
+        double noisySineValue = sineValue + generateGaussianNoise(mean, stdDev);
+        fprintf(fptr, "%.1f,%.8f\n", i, noisySineValue);
     }
     fclose(fptr);
 }
